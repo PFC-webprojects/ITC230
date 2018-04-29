8adee880e4c8706a5d60e2e492e1f7982f9b6715
PFC-webprojects <33673932+PFC-webprojects@users.noreply.github.com>
2018-04-29 15:24:21 -0700

    Week 2 - Let's get modular

--- ListManager/index.js
+++ ListManager/index.js
@@ -1,22 +1,54 @@
-/*  Week 1 - Node.js up and running
+/*  Week 2 - Let's get modular
+ *  index.js
  *  Peter Caliandro
  *  ITC 230, Spring 2018
  */
 
-var fs   = require("fs");
-var http = require("http");
-http.createServer(server).listen(process.env.PORT || 3000);
-console.log("Server started on localhost:3000.  Press <Ctrl+C> to terminate.");  //  Confirm that the server is up and running.
-
-/*  This function is a very simple Node.js web server.
- *  It serves just three web pages: home (index), about, and (404) notfound.
- *  All of these are static web pages.
+/*  This module serves as a Node.js web server to enable access to the skiAreas database.
  */
-function server(request, response) {
-    var path = request.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    var contentType;
-    var statusCode;
+
+const fs          = require("fs");
+const http        = require("http");
+const queryString = require("querystring");
+
+const httpStatus  = require("http-status-codes");  //  This module has been downloaded from NPM and saved to this project.
+const skiAreas    = require("./lib/skiareas");  //  This module was custom-built for this project.
+
+
+let server = function(request, response) {
+    let originalURL     = request.url;
+    let path            = originalURL.replace(/\/?(?:\?.*)?$/, '').toLowerCase();  //  to enable case-insensitive routing
+    let responseContent = "";  //  This default value will be tested for later on.
     
+    const CONTENT_HTML  = "text/html";
+    const CONTENT_PLAIN = "text/plain";
+    let   contentType;  //  will usually be either "text/html" or "text/plain".
+    let   statusCode;  //  This will be one of the constants pre-defined in the http-status-codes NPM module.
+
+    let sendResponse    = function(code, type, content) {
+        response.writeHead(code, { "Content-Type" : type });
+        response.end(content);
+    };  //  function for sending a web page to the client based on parameters that will be set mostly in the switch block below
+
+    const GET           = "/get";
+    const DELETE        = "/delete";
+    const ADD           = "/add";
+    let   query         = function(queryType, URL) {
+        return  queryString.parse(URL.replace(new RegExp(queryType + "\\?", "i"), ""));
+    };  //  Remove the path (such as GET or DELETE as defined above) and the question mark from the start of the query string.
+        //  Then parse the remainder of the query string into an object.
+    let   searchTerm;
+    let   searchResult;
+
+    const NOT_FOUND_1   = " was not found.\n\n\n";
+    const NOT_FOUND_2   = "Make sure that you enter all applicable query terms, " +
+                          "that you spell everything correctly, " +
+                          "and that you apply the correct separators between terms.\n\n" +
+                          "Also note that all field names must be entered " +
+                          "in all lowercase letters.";
+    const NO_NEW        = "No new ski area has been added.\n\n";
+    const DETAILS       = "with the following details:\n\n";
+
     switch(path) {  //  Depending on the original value for path, reset the path, and set the contentType and the statusCode.
         case "":
         case "/home.html":
@@ -24,29 +56,94 @@ function server(request, response) {
         case "/index.html":
         case "/index":
             path        = "/public/home.html";
-            contentType = "text/html";
-            statusCode  = 200;
+            contentType = CONTENT_HTML;
+            statusCode  = httpStatus.OK;
             break;
         case "/about.html":
         case "/about":
             path        = "/public/about.html";
-            contentType = "text/html";
-            statusCode  = 200;
+            contentType = CONTENT_HTML;
+            statusCode  = httpStatus.OK;
+            break;
+        case GET:
+            path             = "";  //  This route will not result in a pre-written HTML page being displayed.
+            searchTerm       = query(GET, originalURL).name;  //  Extract the search term from the query string.
+            responseContent  = "Here is information about " + searchTerm + ":\n\n";
+            searchResult     = skiAreas.get(searchTerm);  //  Look up searchTerm among the ski areas.
+            responseContent += searchResult ? JSON.stringify(searchResult) :
+                               searchTerm + NOT_FOUND_1 + NOT_FOUND_2;
+                               //  searchResult will be undefined if name was undefined or otherwise not found among the skiAreas
+            contentType      = CONTENT_PLAIN;
+            statusCode       = searchResult ? httpStatus.OK : httpStatus.BAD_REQUEST;
+            break;
+        case DELETE:
+            path             = "";  //  This route will not result in a pre-written HTML page being displayed.
+            searchTerm       = query(DELETE, originalURL).name;  //  Extract the search term from the query string.
+            responseContent  = "The record for " + searchTerm + " . . . ";
+            searchResult     = skiAreas.delete(searchTerm);  //  Look up searchTerm among the ski areas.
+            responseContent += searchResult ? " has been deleted." : NOT_FOUND_1 + NOT_FOUND_2;
+                               //  searchResult will be undefined if name was undefined or otherwise not found among the skiAreas
+            contentType      = CONTENT_PLAIN;
+            statusCode       = searchResult ? httpStatus.OK : httpStatus.BAD_REQUEST;
+            break;
+        case ADD:
+            path             = "";  //  This route will not result in a pre-written HTML page being displayed.
+            let name = query(ADD, originalURL).name;  //  Extract the ski area's name from the query string.
+            if (name === undefined) {  //  No ski area name could be found in the query string.
+                responseContent = "A name field must be included in the query string.\n" +
+                                  NO_NEW + NOT_FOUND_2;
+                statusCode      = httpStatus.BAD_REQUEST;
+            } else {
+                let newSkiArea = skiAreas.add(
+                    name,
+                    query(ADD, originalURL).region,
+                    query(ADD, originalURL).country,
+                    query(ADD, originalURL).latitude,
+                    query(ADD, originalURL).top,
+                    query(ADD, originalURL).base,
+                    query(ADD, originalURL).website,
+                    query(ADD, originalURL).article
+                );
+                if (newSkiArea === undefined) {
+                    responseContent = "The ski area " + name + " is already in the list, " +
+                                      DETAILS +
+                                      JSON.stringify(skiAreas.get(name)) + "\n\n" +
+                                      NO_NEW;
+                    statusCode      = httpStatus.BAD_REQUEST;
+                } else {
+                    responseContent = "The ski area " + name + " has been added, " +
+                                      DETAILS + JSON.stringify(newSkiArea);
+                    statusCode      = httpStatus.OK;
+                }
+            }
+            contentType = CONTENT_PLAIN;
             break;
         default:
             path        = "/public/notfound.html";
-            contentType = "text/html";
-            statusCode  = 404;
+            contentType = CONTENT_HTML;
+            statusCode  = httpStatus.NOT_FOUND;
             break;
     }
 
-    fs.readFile(__dirname + path, function(err, content) {
-        if (err) {
-            contentType =  "text/plain";
-            statusCode  =  500;
-            content     =  statusCode + ": Internal Error";
-        }
-        response.writeHead(statusCode, { "Content-Type" : contentType });
-        response.end(content);
-    });
-} 
+
+    if (responseContent) {  //  then display that content
+        sendResponse(statusCode, contentType, responseContent);
+    } else {  //  look up and display an html page
+        fs.readFile(__dirname + path, function(err, fileContent) {
+            //  Note to self:  Watch out.  From here, this function completes asynchronously.
+            if (err) {
+                contentType      =  CONTENT_PLAIN;
+                statusCode       =  httpStatus.INTERNAL_SERVER_ERROR;
+                responseContent  =  statusCode + ": Internal Error";
+            } else {  //  contentType and statusCode should have already been set by now
+                responseContent = fileContent;
+            }
+            sendResponse(statusCode, contentType, responseContent);
+        });
+    }
+        
+}; 
+
+
+http.createServer(server).listen(process.env.PORT || 3000);
+console.log("Server started on localhost:3000.  Press <Ctrl+C> to terminate.");  //  Confirm that the server is up and running.
---
+++ ListManager/lib/skiareas.js
@@ -0,0 +1,148 @@
+/*  Week 2 - Let's get modular
+ *  skiareas.js
+ *  Peter Caliandro
+ *  ITC 230, Spring 2018
+ */
+
+/*  This module creates and stores a makeshift, static database of ski areas
+ *  and exposes methods for accessing and temporarily manipulating its information.
+ *  Changes made using those methods will not be saved anywhere.
+ */
+
+const source =
+`Shymbulak Mountain Resort	Zaiilisky Alatau	Kazakhstan	43	3200	2200	http://www.shymbulak.com/	https://en.wikipedia.org/wiki/Shymbulak
+Afriski	Maluti Mountains	Lesotho	-29	3222	2917	https://www.afriski.net/	https://en.wikipedia.org/wiki/Afriski
+Mt Olympus Ski Resort	Troodos Mountains	Cyprus	35	1930	1850	http://www.cyprusski.com/	http://www.snow-forecast.com/resorts/Mt-Olympus
+Mzaar Kfardebian	Mount Lebanon Range	Lebanon	34	2465	1850	http://www.mzaarskiresort.co/mzaar/	https://en.wikipedia.org/wiki/Mzaar_Kfardebian
+Tiffindell	Drakensberg	South Africa	-31	2900	2720	http://www.tiffindell.co.za/	https://en.wikipedia.org/wiki/Tiffindell_Ski_Resort
+Khoshakoo	West Azerbaijan Province	Iran	37	2200	2000		https://skiofpersia.com/en/resort/khoshakoo/
+Masikryong Ski Resort	Masikryong Range	North Korea	39	1360	768		https://en.wikipedia.org/wiki/Masikryong_Ski_Resort
+Yabuli Ski Resort	Changbai Mountain Range	China	44	1345	397		https://www.chinahighlights.com/harbin/attraction/yabuli-skiing-center.htm
+Tawang	Arunachal Pradesh	India	28		3048		https://www.tourism-of-india.com/skiing-in-tawang.html
+Cerro Castor	Tierra del Fuego Province	Argentina	-55	1057		http://www.cerrocastor.com/	https://en.wikipedia.org/wiki/Cerro_Castor
+Tsaghkadzor Ski Resort	Kotayk Province	Armenia	41	2820	1966	http://www.winterarmenia.com/skiinfo.php#mountain	https://en.wikipedia.org/wiki/Tsaghkadzor_ski_resort
+Perisher Ski Resort	Snowy Mountains	Australia	-36	2054	1720	https://www.perisher.com.au/	https://en.wikipedia.org/wiki/Perisher_Ski_Resort
+Oukaimeden	Atlas Mountains	Morocco	31	3200	2600		https://www.lonelyplanet.com/morocco/oukaimeden/activities/full-day-skiing-at-oukaimeden-resort-from-marrakech/a/pa-act/v-8248P23/1316544
+Sisorarfiit - Nuuk	City of Nuuk	Greenland	64	400	10		https://www.tout-sur-google-earth.com/t20058-stations-de-ski-insolites-et-improbables
+Chimgan	Tian Shan	Uzbekistan	41	3309			https://orexca.com/ski_tours_chimgan.shtml
+Monterreal	Sierra Madre Oriental	Mexico	25	3445	2940		http://www.monterreal.com/
+Zavjalikha	Ural Mountains	Russia	55	840	414	https://zavjalikha.net/	https://www.onthesnow.com/russia/ski-resorts.html
+Solnechnaya Dolina Mountain Ski Center	City of Minsk	Belarus	54		281	https://yesbelarus.com/activities/skiing-skating/solnechnaya-dolina-mountain-ski-center/	http://www.skiingaroundtheworldbook.com/skiing-belarus/
+Cloudmont Ski & Golf Resort	Southern Appalachians	United States	35	549	503	http://www.cloudmont.com/	https://www.circlesquarediamond.com/blogs/news/91670214-10-unique-ski-resorts-you-ve-likely-never-heard-of
+Sky Resort	Bogd Khan Mountain	Mongolia	48	1570	1379	http://www.skyresort.mn/pages/%D0%B1%D0%B0%D0%B0%D0%B7%D1%8B%D0%BD-%D1%82%D0%B0%D0%BD%D0%B8%D0%BB%D1%86%D1%83%D1%83%D0%BB%D0%B3%D0%B0	`;
+//  This source string serves as the raw data from which to create a makeshift, static database.
+//  Note that this source string contains newlines ("\n") and tabs ("\t").
+//  (It was simply copied from an Excel worksheet left over from a previous web development school project.)
+
+
+let skiArea = function(  //  This function creates a SkiArea object from a set of parameters.
+        name,  //  name is the only required parameter.  It will also be used later on as a unique key in the skiAreas array.
+        region = "",
+        country = "",
+        latitude = NaN,  //  We're using NaN here as a numeric empty-string equivalent,
+        top = NaN,  //  as the value of 0 has a numeric meaning that would be inaccurate
+        base = NaN,  //  and misleading in this context.
+        website = "",
+        article = ""
+    ){
+    return {
+        "Name"     : name,
+        "Region"   : region,
+        "Country"  : country,
+        "Latitude" : latitude,
+        "Top"      : top,
+        "Base"     : base,
+        "Website"  : website,
+        "Article"  : article
+    };
+};
+
+
+//  This function creates an array of skiArea objects to serve as a makeshift database.
+//  It gets its data from the source string defined above.
+//  
+//  Split the source string by the newline character to create an array of rows.
+//  Then split each row by the tab character to create an array of columns.
+//  Then create a skiArea object from each row by assigning each column to its own field.
+let skiAreas = source.split("\n").map(row => {
+    let columns = row.split("\t");
+    return skiArea(
+                 columns[0] ,
+                 columns[1] ,
+                 columns[2] ,
+        parseInt(columns[3]),
+        parseInt(columns[4]),
+        parseInt(columns[5]),
+                 columns[6] ,
+                 columns[7]
+    );
+});
+
+
+exports.getAll = function() {  //  Return the entire skiAreas array, sorted by name.
+    return skiAreas.sort((first, second) =>
+        first.Name.localeCompare(second.Name)
+    );
+};
+
+
+exports.get = function(name) {  //  Return one skiArea object.  Search for it by name.
+    if (!name) {  //  In case an empty string is passed as a parameter . . .
+        return undefined;  //  A return value of undefined means that no skiArea was found.
+    }
+
+    name = name.toLowerCase();  //  to enable case-insensitive searching
+    return skiAreas.find(skiArea =>
+        skiArea.Name.toLowerCase() === name  //  Returns undefined if name is not found.
+    );
+};
+
+
+exports.delete = function(name) {  //  It is assumed that name will be a string.
+    if (!name) {  //  In case an empty string is passed . . .
+        return undefined;  //  A return value of undefined means that no skiArea was found.
+    }
+
+    name = name.toLowerCase();  //  to enable case-insensitive searching
+    let index = skiAreas.findIndex(skiArea =>
+        skiArea.Name.toLowerCase() === name
+    );
+    if (index === -1) {  //  Not found; return a value to indicate that.
+        return undefined;
+    } else {  //  Found.  Delete the object from the array.  Return that object to the caller.
+        return skiAreas.splice(index, 1);
+    }
+    
+};
+
+
+//  If it is not already in the skiAreas array, create a new skiArea from the given parameters, and add it to skiAreas.
+//  Return the new skiArea object -- but if name is already in skiAreas, then don't create any new object; return undefined instead.
+exports.add = function(
+        name,
+        region = "",
+        country = "",
+        latitude = NaN,
+        top = NaN,
+        base = NaN,
+        website = "",
+        article = ""
+    ) {
+    if (exports.get(name)) {  //  then a ski area with the name name is already in the skiAreas array.
+        return undefined;  //  to indicate that we will not be adding this ski area a second time.
+    }
+    
+    let newSkiArea = skiArea(
+        name,
+        region,
+        country,
+        parseInt(latitude),
+        parseInt(top),
+        parseInt(base),
+        website,
+        article
+    );
+    skiAreas.push(newSkiArea);
+    
+    return newSkiArea;  //  to confirm that this new skiArea has been added to the skiAreas array.
+};
